53e5091059a17146cfc281297f9dbf48038a6b12
Riley Collard <91252297+Ethyx07@users.noreply.github.com>
2026-08-25 23:35:01 +1000

    setup basic peer to peer connection and sync

--- cardslop/Test/main.gd
+++ cardslop/Test/main.gd
@@ -1,16 +1,65 @@
 extends Node3D
 
+const PLAYERCONTROLLER = preload("uid://cl4xivdhhcbae")
 
-# Called when the node enters the scene tree for the first time.
+var players : Array[CharacterBody3D]
+
 func _ready() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	Networking.host_created.connect(on_host_created)
+	
+	$MultiplayerSpawner.spawn_function = spawn_player_from_data
+	
+
+func on_host_created() -> void:
+	#Spawns host player
+	spawn_player(multiplayer.get_unique_id())
+	multiplayer.peer_connected.connect(spawn_player)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func spawn_player(peer_id : int) -> void:	
+	if not multiplayer.is_server():
+		return
+	
+	var spawn_data := {
+		"peer_id": peer_id,
+		"position": $SpawnPoint.global_position
+	}
+	
+	$MultiplayerSpawner.spawn(spawn_data)
+
+func spawn_player_from_data(data : Dictionary) -> Node:
+	var new_player := PLAYERCONTROLLER.instantiate() as CharacterBody3D
+	
+	new_player.name = str(data["peer_id"])
+	new_player.global_position = data["position"]
+	
+	return new_player
+func initialise_player(player : CharacterBody3D) -> void:	
+	for other in players:
+		player.add_collision_exception_with(other)
+		other.add_collision_exception_with(player)
+	players.append(player)
 
 
+func _on_host_pressed() -> void:
+	Networking.host_lobby()
+	
+	if not multiplayer.is_server():
+		for child in get_children():
+			if child is CharacterBody3D:
+				$DebugLabel.text = (
+					"Peer: " + str(multiplayer.get_unique_id()) +
+					"\nPlayer: " + child.name +
+					"\nPlayer pos: " + str(child.global_position) +
+					"\nSpawn pos: " + str($SpawnPoint.global_position)
+				)
+
+func _on_multiplayer_spawner_spawned(node: Node) -> void:
+	if node is CharacterBody3D:
+		initialise_player(node)
+		
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_cancel"):
+	if event.is_action_pressed("Quit"):
 		get_tree().quit()
+	elif event.is_action_pressed("Host"):
+		Networking.host_lobby()
